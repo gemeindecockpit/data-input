52e07b31f80b4d0bc7270479076f963a16c01552
pkruse <pa.kruse@ostfalia.de>
2020-08-02 21:44:24 +0200

    wip: add structure for getDerivedStateFromProps
    componentWillReceiveProps is unsafe and will not be supported in react 17 so I started to alter the LCM accordingly

--- src/OrganisationViewer.js
+++ src/OrganisationViewer.js
@@ -2,17 +2,47 @@ import React from "react";
 import OrganisationList from "./OrganisationList";
 import ProxyJSON from "./ProxyJSON";
 
-const temporaryJson = ProxyJSON()
+let temporaryJson = ProxyJSON();
 
 export default class OrganisationViewer extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state={organisations:temporaryJson.organisations};
+    }
+
+    componentDidMount() {
+        /* RemoteApiCall*/
+    }
+
+    // componentWillReceiveProps(nextProps) {
+    //     if (nextProps.organisations!==this.state.organisations){
+    //         this.setState({organsisations:nextProps.organisations})
+    //     }
+    // }
+    static getDerivedStateFromProps(nextProps, prevState){
+
+        return null;
+        // if(nextProps.organisations!==prevState.organisations){
+        //     return {organisations : nextProps.organisations};
+        // }
+        // else return null;
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if(prevState.organisations!==this.state.organisations){
+            this.setState({organisations:prevState.organisations})
+        }
+    }
+
     chooseOrganisation = (orgID) => {
-        this.props.chosenOrganisation(orgID)
-        console.log('print from Viewer' + orgID)
+        this.props.chosenOrganisation(orgID);
+        console.log('print from Viewer' + orgID);
     }
 
     render() {
         return (
-            <OrganisationList chosenOrganisation={this.chooseOrganisation} data={temporaryJson.organisations}/>
-        )
+            <OrganisationList chosenOrganisation={this.chooseOrganisation} data={this.state.organisations}/>
+        );
     }
 }
