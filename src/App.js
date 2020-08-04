0ba8de0459565458ed9d8f32d05da0f4ed05aa41
Tobias Gersdorf <t.gersdorf@ostfalia.de>
2020-08-04 15:26:31 +0200

    Update Header import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Datepicker from './Datepicker/Datepicker'
 import ContentRouter from './ContentRouter';
-import Header from "./Header"
+import Header from "./Header/Header"
 import OrganisationViewer from "./OrganisationViewer";
 import Icon from '@material-ui/core';
 import ProxyJSON from "./ProxyJSON";
@@ -29,7 +29,6 @@ class App extends React.Component {
           <Header chosenDate={this.state.chosenDate}></Header>
             <ContentRouter />
             <Datepicker onChange={this.onDateChange.bind(this)} label="Datum auswählen:"></Datepicker>
-
             <OrganisationViewer chosenOrganisation={this.loadOrganisation} organisations={temporaryJSON.organisations}/>
         </div>
 
