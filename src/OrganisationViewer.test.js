ed7253f2e63ee0f8d28b8c117fbb7c11ab98afda
plkrusePTB <47141905+plkrusePTB@users.noreply.github.com>
2020-08-05 07:23:07 +0200

    wip(.gitlab-ci.yml): corrected typo

--- src/OrganisationViewer.test.js
+++ src/OrganisationViewer.test.js
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react';
-import OrganisatinViewer from './OrganisationViewer';
+import OrganisationViewer from './OrganisationViewer';
 import {isElement} from 'react-dom/test-utils'
 import { is } from 'date-fns/locale';
 
