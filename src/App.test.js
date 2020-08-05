73aafa2c7fc7193ad7a22c68e52a39966d0c496e
plkrusePTB <47141905+plkrusePTB@users.noreply.github.com>
2020-08-05 08:54:47 +0200

    feature(App.test.js): integrate automatic tests in pipeline

--- src/App.test.js
+++ src/App.test.js
@@ -3,12 +3,13 @@ import { render } from '@testing-library/react';
 import App from './App';
 import {isElement} from 'react-dom/test-utils'
 import { is } from 'date-fns/locale';
+import ReactDOM from 'react-dom'
 
 test('Is App react?', () => {
   isElement(<App/>)
 });
 
 it('renders without crashing', () => {
-  constdiv = document.createElement('div');
+  const div = document.createElement('div');
   ReactDOM.render(<App/>, div)
 })
--- src/OrganisationViewer.test.js
+++ src/OrganisationViewer.test.js
@@ -1,8 +1,17 @@
 import { render } from '@testing-library/react';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import OrganisationViewer from './OrganisationViewer';
+import ProxyJSON from './ProxyJSON'
 import {isElement} from 'react-dom/test-utils'
 import { is } from 'date-fns/locale';
 
 test('Is OrganisationViewer react element?', () => {
   isElement(<OrganisationViewer/>)
 });
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<OrganisationViewer/>, div)
+  })
+  
