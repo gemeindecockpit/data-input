3a58713ab508e5b65d100ffdf237079fa86078b7
Jannik Recklies <Recklies@janniks-mbp-2.speedport.ip>
2020-08-11 15:51:21 +0200

    Fix: Header in ReviewScreen is now always in one line.

--- src/show-information/ReviewScreen/ReviewScreen.js
+++ src/show-information/ReviewScreen/ReviewScreen.js
@@ -8,7 +8,7 @@ const Divider = ( text ) => {
     return (
         <div style={{ display: "flex", alignItems: "center", paddingLeft: "30px", paddingRight: "30px", paddingTop: "30px" }}>
             <div style={{ borderBottom: "1px solid #FFFFFF80", width: "50%" }} />
-                <span style={{padding: "0 10px 0 10px", color: "white"}}>
+                <span style={{padding: "0 10px 0 10px", color: "white", whiteSpace: "pre"}}>
                     {text}
                 </span>
             <div style={{ borderBottom: "1px solid #FFFFFF80", width: "50%" }} />
