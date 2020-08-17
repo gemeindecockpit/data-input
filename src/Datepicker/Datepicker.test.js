30673ffbe9a831a3d6338553d69e4109f687a1ed
Clarissa Röhrdanz <c.roehrdanz@ostfalia.de>
2020-08-17 22:15:40 +0200

    fireEvent and jest to simulate button clicking

--- src/Datepicker/Datepicker.test.js
+++ src/Datepicker/Datepicker.test.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import Datepicker from './Datepicker.js';
-import renderer from 'react-test-renderer';
 import ReactDOM from 'react-dom';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 
 
 import {isElement} from 'react-dom/test-utils'
@@ -16,14 +15,21 @@ test('Is Datepicker react?', () => {
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  const onDateChange = (date) => {
-
-  }
-  ReactDOM.render(<Datepicker onDateChange={onDateChange} />, div)
+  /*onDateChange = (date) => {
+      console.log("hallo bitte " + date);
+  }*/
+  const onDateChange = jest.fn();
+  render(<Datepicker onDateChange={onDateChange} />, div)
+  fireEvent.click(screen.getByRole('button'));
+  fireEvent.click(screen.getAllByText('1', {exact: true})[1].closest('button'));
+  screen.debug(screen.getAllByText('1', {exact: true}).pop().closest('button'));
+  expect(onDateChange).toHaveBeenCalled();
+  expect(1).toBe(1);
+  //screen.debug();
 })
 
 it('blah', () => {
-    const container = render(<Datepicker onDateChange={onDateChange} />);
+    //const container = render(<Datepicker onDateChange={onDateChange} />);
 
 
 })
