5fac5fcc707341537523ed36d3e48c38eff909ef
Tobias Gersdorf <t.gersdorf@ostfalia.de>
2020-08-18 19:45:03 +0200

    wip add test

---
+++ src/tests/OverviewScreen.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import OverviewScreen from "../pages/datepicker/OverviewScreen";
+
+let container
+
+beforeEach(() => {
+    console.log("WAS IST HEIR LOS")
+});
+
+afterEach(() => {
+    console.log("WARUM")
+})
+
+it('can render and show record', () => {
+    act(() => {
+        // ReactDOM.render(<OverviewScreen />, container)
+    })
+})
