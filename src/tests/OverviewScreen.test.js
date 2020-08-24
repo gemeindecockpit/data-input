0c313a9019f2740ebc69d8e1f750c92cc5245b07
Tobias Gersdorf <t.gersdorf@ostfalia.de>
2020-08-24 20:18:20 +0200

    wip OverviewScreen.test
    todo check correct values for selected date

--- src/tests/OverviewScreen.test.js
+++ src/tests/OverviewScreen.test.js
@@ -1,20 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { act } from 'react-dom/test-utils';
+import {act, isElement} from 'react-dom/test-utils';
 import OverviewScreen from "../pages/datepicker/OverviewScreen";
+import {getOrganisationById, getValuesByOrgIdAndDate} from '../utils/communication/ProxyJSON'
 
-let container
+let container,
+    data_20200807
 
-beforeEach(() => {
-    console.log("WAS IST HEIR LOS")
+beforeAll(() => {
+    container = document.createElement('div')
+    document.body.appendChild(container)
+    data_20200807 = getValuesByOrgIdAndDate("1", new Date("2020-08-07"))
 });
 
-afterEach(() => {
-    console.log("WARUM")
-})
+afterAll(() => {
+    document.body.removeChild(container)
+    container = null
+    data_20200807 = null
+});
 
-it('can render and show record', () => {
-    act(() => {
-        // ReactDOM.render(<OverviewScreen />, container)
+describe('Testing OverviewScreen component', () => {
+
+    it('Is react element', () => {
+        isElement(<OverviewScreen />)
     })
-})
+
+    it('can render and show record', () => {
+        act(() => {
+            ReactDOM.render(<OverviewScreen match={ {
+                params: {
+                    orgId: "1"
+                }
+            } }/>, container)
+        })
+    })
+
+    it('displays correct data for date', () => {
+        console.log(container.querySelector('Datepicker'))
+    })
+
+});
+
+
