ca83663f3c773853f89e9558f79974fa1820ffa4
pkruse <pa.kruse@ostfalia.de>
2020-10-06 22:01:33 +0200

    wip: added post support for data with credentials

--- src/utils/communication/ApiCalls.js
+++ src/utils/communication/ApiCalls.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import axios from 'axios';
 
-//const baseUrl = "http://litwinow.xyz/";
-const baseUrl = "http://localhost:8080/";
+const baseUrl = "https://www.litwinow.xyz/";
+//const baseUrl = "http://localhost:8080/";
 
 export default class ApiCalls extends React.Component {
+    testApiCall = axios.create(
+        {
+            method: 'post',
+            baseURL: baseUrl,
+            data: {
+                "name":"admin",
+                "pass":"password"
+            },
+            headers: {
+                "Content-Type": "multipart/form-data"
+            },
+            withCredentials: true
+        }
+    );
 
-
+    newlogin = () => {
+        var formData = new FormData();
+        formData.append("name", "admin");
+        formData.append("pass", "password");
+        this.testApiCall.request(
+            formData
+                );
+    }
     login = (username, password) => {
+        console.log(this.testApiCall);
         var formData = new FormData();
         formData.append("name", username);
         formData.append("pass", password);
@@ -26,10 +48,11 @@ export default class ApiCalls extends React.Component {
     }
 
     postKpisByOrgId = (orgId, jsonValues) => {
-        return axios.post(baseUrl + "organisation/" + orgId, jsonValues, {
+        return axios.post(baseUrl + "data/organisation/" + orgId, jsonValues, {
             headers: {
-                "Content-Type": "application/json"
-            }
+                "Content-Type": "text/plain"
+            },
+            withCredentials: true
         });
     }
 
